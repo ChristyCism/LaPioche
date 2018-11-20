1a9c40209958a8f03fb6e1f4825ac3baa4a095ae
Annabelle PELLIER <annabplr@gmail.com>
2018-11-20 11:43:54 +0100

    reviews controller routes and views

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,31 @@
+class ReviewsController < ApplicationController
+  before_action :set_reviewee, only: [:new, :create, :index]
+
+  def index
+    @reviews = Review.all
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.reviewee = @reviewee
+    @review.save
+    redirect_to reviewee_reviews_path(@reviewee)
+  end
+
+  def show
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :title, :rating)
+  end
+
+  def set_reviewee
+    @reviewee = Reviewee.find(params[:reviewee_id])
+  end
+end
