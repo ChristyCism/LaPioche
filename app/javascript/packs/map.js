3fe8bb0ac01091accbeec764e87cf1452e297139
Grégoire DIEF <gregoiredief@gmail.com>
2018-11-21 12:39:39 +0100

    on affiche une map et les resultats en moche

---
+++ app/javascript/packs/map.js
@@ -0,0 +1,12 @@
+import 'mapbox-gl/dist/mapbox-gl.css'
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+
+const mapElement = document.getElementById('map');
+
+if (mapElement) { // only build a map if there's a div#map to inject into
+  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10'
+  });
+}
