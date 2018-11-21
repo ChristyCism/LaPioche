1d57ecc6d696d8226e27602cb199f1755fee679c
Grégoire DIEF <gregoiredief@gmail.com>
2018-11-21 16:34:34 +0100

    Worked on the routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,18 +6,4 @@ class PagesController < ApplicationController
     @clients = User.client
   end
 
-  def search
-    if params[:city]
-      @farmers = User.search(params[:city]).farmer
-    else
-      @farmers = User.farmer
-    end
-
-    @markers = @farmers.map do |farmer|
-      {
-        lng: farmer.longitude,
-        lat: farmer.latitude
-      }
-    end
-  end
 end
