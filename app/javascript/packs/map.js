0bc32fef8715697ccf5dbd2603ae57dec952da96
Grégoire DIEF <gregoiredief@gmail.com>
2018-11-21 14:12:24 +0100

    markers on the map

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -2,6 +2,7 @@ import 'mapbox-gl/dist/mapbox-gl.css'
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
 
 const mapElement = document.getElementById('map');
+const markers = JSON.parse(mapElement.dataset.markers);
 
 if (mapElement) { // only build a map if there's a div#map to inject into
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
@@ -9,4 +10,9 @@ if (mapElement) { // only build a map if there's a div#map to inject into
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v10'
   });
+    markers.forEach((marker) => {
+    new mapboxgl.Marker()
+      .setLngLat([marker.lng, marker.lat])
+      .addTo(map);
+});
 }
