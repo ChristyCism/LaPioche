4c1ce9aefad5b9a6767b41d96fdc58227bb47908
Grégoire DIEF <gregoiredief@gmail.com>
2018-11-21 15:21:47 +0100

    avec la page recherche, la map et les avatars

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -15,4 +15,16 @@ if (mapElement) { // only build a map if there's a div#map to inject into
       .setLngLat([marker.lng, marker.lat])
       .addTo(map);
 });
+    if (markers.length === 0) {
+    map.setZoom(1);
+  } else if (markers.length === 1) {
+    map.setZoom(14);
+    map.setCenter([markers[0].lng, markers[0].lat]);
+  } else {
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach((marker) => {
+      bounds.extend([marker.lng, marker.lat]);
+    });
+    map.fitBounds(bounds, { duration: 0, padding: 75 })
+  }
 }
