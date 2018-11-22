2ec681a623b27cab3c202184ebf86f816f9daaaa
Grégoire DIEF <gregoiredief@gmail.com>
2018-11-22 12:27:02 +0100

    re routing done hopefully

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,7 @@
+class BookingsController < ApplicationController
+  def create
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :users, only: [:index] do
+  resources :users, only: [:index, :show] do
     collection do
       get :search
     end
   end
-
-  resources :pages, only: :index do
-    collection do
-      get :search
-    end
-  end
+  resources :messages, only: [:index, :create]
+  resources :bookings, only: [:create, :show]
   resources :reviews, only: [:show, :index, :new, :create]
-  resources :conversations, only: [:index, :create] do
-    resources :messages, only: [:index, :create]
-  end
 end
