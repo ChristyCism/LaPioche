89d13d6278ddc881affbb9399724298b16307012
Christy <valette.christy@gmail.com>
2018-11-23 15:58:48 +0100

    ajouter un message = fonctionnel

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,15 +9,20 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = @conversation.messages.new(message_params)
+
+    @message = Message.new(message_params)
+    @user = User.find(params[:message][:receiver_id].to_i)
     if @message.save
-      redirect_to conversation_messages_path(@conversation)
+      redirect_to user_conversation_path(@user)
+
+    else
+      p @message
     end
   end
 
   private
 
   def message_params
-    params.require(:message).permit(:body, :user_id)
+    params.require(:message).permit(:body, :sender_id, :receiver_id)
   end
 end
