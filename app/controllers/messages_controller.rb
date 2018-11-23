3bfce2396ee0cc0ac3f2ba0e57a836115975554a
Christy <valette.christy@gmail.com>
2018-11-23 17:17:36 +0100

    Invoque god

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,4 @@
 class MessagesController < ApplicationController
-
   def index
     @user1 = current_user
     @user2 = User.find(params[:user_id])
@@ -9,12 +8,11 @@ class MessagesController < ApplicationController
   end
 
   def create
-
     @message = Message.new(message_params)
     @user = User.find(params[:message][:receiver_id].to_i)
+
     if @message.save
       redirect_to user_conversation_path(@user)
-
     else
       p @message
     end
